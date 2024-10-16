e11938fde7ee81ee9b7a67a8f77d0b52bce53c4b
Phillip Orviss <phillip.orviss@hollard.com.au>
2024-10-16 22:45:58 +0200

    Initial commit of base Terraform configuration

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "The project ID to deploy resources in"
+  type        = string
+  default     = "home-management-438820"
+}
+
+variable "region" {
+  description = "Google Cloud region"
+  type        = string
+  default     = "africa-south1"
+}
