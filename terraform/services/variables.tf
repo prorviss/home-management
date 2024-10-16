dcd60f8a82485819a9b9cf0f48ddaf94fffee6fe
Phillip Orviss <phillip.orviss@hollard.com.au>
2024-10-17 01:12:15 +0200

    Added starter for services module

---
+++ terraform/services/variables.tf
@@ -0,0 +1,17 @@
+variable "project_id" {
+  description = "The project ID to deploy resources in"
+  type        = string
+  default     = "home-management-438820"
+}
+
+variable "project_number" {
+  description = "The project number"
+  type        = number
+  default     = 656120491361
+}
+
+variable "region" {
+  description = "Google Cloud region"
+  type        = string
+  default     = "africa-south1"
+}
